9c9adb1097e3b51209ad94fa3f34b5d83dba134c
nhienphan <phanvannhien@gmail.com>
2019-05-16 22:31:01 +0700

    fix screen large

--- src/resources/js/page.js
+++ src/resources/js/page.js
@@ -137,41 +137,30 @@ jQuery(document).ready(function(){
                 breakpoint: 1800,
                 settings: {
                     slidesToShow: 6,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: true
                 }
             },
             {
                 breakpoint: 1400,
                 settings: {
                     slidesToShow: 5,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: true
                 }
             },
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: true
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1
                 }
             }
         ]
